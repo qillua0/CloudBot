948e826acec24f9ae030a2911e6547df6d7f9d51
qillua0 <108080341+qillua0@users.noreply.github.com>
2022-06-23 21:59:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,7 @@ global.sgh = process.env.LINK_GITHUB
 global.sgc = process.env.LINK_GROUP
 global.ownernumber = process.env.NOWNER
 global.owner = [
-  ['0'],
-  ['0'],
+  ['081276709668'],
   [`${process.env.NOWNER}`, `${process.env.NAMAOWNER}`, true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
